c95a3a91e523e056c769a8c0595859969076ace3
dudu <aed6@alu.ua.es>
2022-05-02 13:12:54 +0200

    Add project files.

---
+++ SOM/clasificacionSOM.cu
@@ -0,0 +1,302 @@
+/*----------------------------------------------------------------------------*/
+/*  FICHERO:       clasificacionSOM.cu									        */
+/*  AUTOR:         Jorge Azorin								       			    */
+/*													                            */
+/*  RESUMEN												                        */
+/*  ~~~~~~~												                        */
+/* Ejercicio grupal para la clasificación de patrones de entrada basada         */
+/* en SOM utilizando GPUs                                                       */
+/*----------------------------------------------------------------------------*/
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+
+
+// includes, project
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include "clasificacionSOM.h"
+#include <Windows.h>
+
+
+
+#define ERROR_CHECK { cudaError_t err; if ((err = cudaGetLastError()) != cudaSuccess) { printf("CUDA error: %s, line %d\n", cudaGetErrorString(err), __LINE__);}}
+
+typedef LARGE_INTEGER timeStamp;
+double getTime();
+
+/*----------------------------------------------------------------------------*/
+/*  FUNCION A PARALELIZAR  (versión secuencial-CPU)  				          */
+/*	Implementa la clasificación basada en SOM de un conjunto de patrones      */
+/*  de entrada definidos en un fichero                                         */
+/*----------------------------------------------------------------------------*/
+int ClasificacionSOMCPU()
+{
+	float distancia;
+	float distanciaMenor=MAXDIST;
+
+	for (int np=0;np<Patrones.Cantidad;np++) // Recorrido de todos los patrones
+	{
+		distanciaMenor=MAXDIST;
+		for (int y = 0; y<SOM.Alto; y++)			// Recorrido de todas las neuronas
+		{
+			for (int x = 0; x<SOM.Ancho; x++)
+			{
+				distancia=CalculaDistancia(y,x,np);     // CalculaDistancia entre neurona (y,x) y patrón np
+				for (int vy=-1;vy<2;vy++)               // Calculo en la vecindad
+					for (int vx=-1;vx<2;vx++)
+						if (vx != 0 && vy != 0)         // No comprobar con la misma neurona
+						   distancia+=CalculaDistancia(y+vy,x+vx,np);
+				if (distancia < distanciaMenor)
+				{
+					distanciaMenor=distancia;  // Neurona con menor distancia
+					EtiquetaCPU[np]=SOM.Neurona[y][x].label;
+				}
+
+			}
+		}
+
+	}
+	return OKCLAS;									// Simulación CORRECTA
+}
+
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+// FUNCION A IMPLEMENTAR POR EL GRUPO (paralelización de ClasificacionSOMCPU)
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+
+ int ClasificacionSOMGPU()
+{
+
+
+	 return OKCLAS;
+}
+ // ---------------------------------------------------------------
+ // ---------------------------------------------------------------
+ // ---------------------------------------------------------------
+ // ---------------------------------------------------------------
+ // ---------------------------------------------------------------
+
+ // Declaraciones adelantadas de funciones
+ int LeerSOM(const char *fichero);
+ int LeerPatrones(const char *fichero);
+ 
+
+
+////////////////////////////////////////////////////////////////////////////////
+//PROGRAMA PRINCIPAL
+////////////////////////////////////////////////////////////////////////////////
+void
+runTest(int argc, char** argv)
+{
+
+  	double gpu_start_time, gpu_end_time;
+	double cpu_start_time, cpu_end_time;
+
+	/* Numero de argumentos */
+	if (argc != 3)
+	{
+		fprintf(stderr, "Numero de parametros incorecto\n");
+		fprintf(stderr, "Uso: %s estructura.som patrones.pat\n", argv[0]);
+		return;
+	}
+
+	/* Apertura de Fichero */
+	printf("Clasificacion basada en SOM...\n");
+	/* Mapa SOM */
+	if (LeerSOM((char *)argv[1]) == ERRORCLASS)
+	{
+		fprintf(stderr, "Lectura de SOM incorrecta\n");
+		return;
+	}
+	/* Patrones */
+	if (LeerPatrones((char *)argv[2]) == ERRORCLASS)
+	{
+		fprintf(stderr, "Lectura de patrones incorrecta\n");
+		return;
+	}
+	
+	// Creación etiquetas resultados para versiones CPU y GPU
+
+	EtiquetaCPU = (int*)malloc(Patrones.Cantidad*sizeof(int));
+	EtiquetaGPU = (int*)malloc(Patrones.Cantidad*sizeof(int));
+	
+	/* Algoritmo a paralelizar */
+	cpu_start_time = getTime();
+	if (ClasificacionSOMCPU() == ERRORCLASS)
+	{
+		fprintf(stderr, "Clasificación CPU incorrecta\n");
+		BorrarMapa();
+		if (EtiquetaCPU != NULL) free(EtiquetaCPU);
+		if (EtiquetaGPU != NULL) free(EtiquetaCPU);
+		exit(1);
+	}
+	cpu_end_time = getTime();
+	cudaSetDevice(0);
+	/* Algoritmo a implementar */
+	gpu_start_time = getTime();
+	if (ClasificacionSOMGPU() == ERRORCLASS)
+	{
+		fprintf(stderr, "Clasificación GPU incorrecta\n");
+		BorrarMapa();
+		if (EtiquetaCPU != NULL) free(EtiquetaCPU);
+		if (EtiquetaGPU != NULL) free(EtiquetaGPU);
+		return;
+	}
+	cudaDeviceSynchronize();
+	gpu_end_time = getTime();
+	// Comparación de corrección
+	int comprobar = OKCLAS;
+	for (int i = 0; i<Patrones.Cantidad; i++)
+	{
+		if ((EtiquetaCPU[i] != EtiquetaGPU[i]))
+		{
+			comprobar = ERRORCLASS;
+			fprintf(stderr, "Fallo en la clasificacion del patron %d, valor correcto %d\n", i, EtiquetaCPU[i]);
+		}
+	}
+	// Impresion de resultados
+	if (comprobar == OKCLAS)
+	{
+		printf("Clasificacion correcta!\n");
+
+	}
+	// Impresión de resultados
+	printf("Tiempo ejecución GPU : %fs\n", \
+		gpu_end_time - gpu_start_time);
+	printf("Tiempo de ejecución en la CPU : %fs\n", \
+		cpu_end_time - cpu_start_time);
+	printf("Se ha conseguido un factor de aceleración %fx utilizando CUDA\n", (cpu_end_time - cpu_start_time) / (gpu_end_time - gpu_start_time));
+	// Limpieza de Neuronas
+	BorrarMapa();
+	BorrarPatrones();
+	if (EtiquetaCPU != NULL) free(EtiquetaCPU);
+	if (EtiquetaGPU != NULL) free(EtiquetaGPU);
+	return;
+}
+
+int
+main(int argc, char** argv)
+{
+	runTest(argc, argv);
+	getchar();
+}
+
+/* Funciones auxiliares */
+double getTime()
+{
+	timeStamp start;
+	timeStamp dwFreq;
+	QueryPerformanceFrequency(&dwFreq);
+	QueryPerformanceCounter(&start);
+	return double(start.QuadPart) / double(dwFreq.QuadPart);
+}
+
+
+
+/*----------------------------------------------------------------------------*/
+/*	Función:  LeerSOM(char *fichero)						              */
+/*													                          */
+/*	          Lee la estructura del SOM con formato .SOM   */
+/*----------------------------------------------------------------------------*/
+int LeerSOM(const char *fichero)
+{
+	int i, j, ndim, count;		/* Variables de bucle */
+	int alto,ancho;		/* Variables de tamaño del mapa */
+	FILE *fpin; 			/* Fichero */
+	int nx,ny,lx,ly,label,dimension;
+	float pesos;
+
+	/* Apertura de Fichero */
+	if ((fpin = fopen(fichero, "r")) == NULL) return ERRORCLASS;
+	/* Lectura de cabecera */
+	if (fscanf(fpin, "Alto: %d\n", &alto)<0) return ERRORCLASS;
+	if (fscanf(fpin, "Ancho: %d\n", &ancho)<0) return ERRORCLASS;
+	if (fscanf(fpin, "Dimension: %d\n", &dimension)<0) return ERRORCLASS;
+	if (feof(fpin)) return ERRORCLASS;
+
+	if (CrearMapa(alto, ancho, dimension) == ERRORCLASS) return ERRORCLASS;
+	/* Lectura del SOM */
+	count = 0;
+	for (i = 0; i<ancho; i++)
+	{
+		for (j = 0; j<alto; j++)
+		{
+			if (!feof(fpin))
+			{
+				fscanf(fpin, "N%d,%d:", &nx, &ny);
+				for (ndim = 0; ndim<dimension-1;ndim++)
+				{
+			        fscanf(fpin, " %f", &pesos);
+				    SOM.Neurona[j][i].pesos[ndim] = pesos;
+				}
+				fscanf(fpin, " %f\n", &pesos);
+				SOM.Neurona[j][i].pesos[ndim] = pesos;
+				
+				fscanf(fpin, "L%d,%d: %d\n", &lx, &ly, &label);
+				SOM.Neurona[j][i].label=label;
+			
+				count++;
+			}
+			else break;
+		}
+	}
+	fclose(fpin);
+	if (count != ancho*alto) return ERRORCLASS;
+	return OKCLAS;
+}
+
+/*----------------------------------------------------------------------------*/
+/*	Función:  LeerPatrones(char *fichero)						              */
+/*													                          */
+/*	          Lee los patrones de un fichero de entrada .pat   */
+/*----------------------------------------------------------------------------*/
+int LeerPatrones(const char *fichero)
+{
+	int i, ndim, count;		/* Variables de bucle */
+	int cantidad,dimension;		/* Variables de tamaño de los patrones */
+	FILE *fpin; 			/* Fichero */
+
+	int np;
+	float pesos;
+
+	/* Apertura de Fichero */
+	if ((fpin = fopen(fichero, "r")) == NULL) return ERRORCLASS;
+	/* Lectura de cabecera */
+	if (fscanf(fpin, "Numero: %d\n", &cantidad)<0) return ERRORCLASS;
+	if (fscanf(fpin, "Dimension: %d\n", &dimension)<0) return ERRORCLASS;
+	if (feof(fpin)) return ERRORCLASS;
+	
+	if (CrearPatrones(cantidad, dimension) == ERRORCLASS) return ERRORCLASS;
+	/* Lectura de patrones */
+	count = 0;
+	for (i = 0; i<cantidad; i++)
+	{
+	  	if (!feof(fpin))
+		{
+			fscanf(fpin, "P%d:", &np);
+				for (ndim = 0; ndim<dimension-1;ndim++)
+				{
+			        fscanf(fpin, " %f", &pesos);
+					Patrones.Pesos[i][ndim] = pesos;
+				}
+				fscanf(fpin, " %f\n", &pesos);
+				Patrones.Pesos[i][ndim] = pesos;
+				
+					
+				count++;
+			}
+			else break;
+	}
+	
+	fclose(fpin);
+	if (count != cantidad) return ERRORCLASS;
+	return OKCLAS;
+}
+
---
+++ SOM/clasificacionSOM.h
@@ -0,0 +1,141 @@
+/*----------------------------------------------------------------------------*/
+/*  FICHERO:       clasificacionSOM.h									          */
+/*  AUTOR:         Jorge Azorin											  */
+/*													                          */
+/*  RESUMEN												                      */
+/*  ~~~~~~~												                      */
+/* Fichero de definiciones y estructuras                                      */
+/*    						                                                  */
+/*----------------------------------------------------------------------------*/
+
+#ifndef _CLASIFICASOM_H_
+#define _CLASIFICASOM_H_
+
+/*============================================================================ */
+/* Constantes											                       */
+/*============================================================================ */
+#define ERRORCLASS 1
+#define OKCLAS    0
+#define MAXDIST 10000.0
+
+/*============================================================================ */
+/* Estructuras											                       */
+/*============================================================================ */
+
+	struct sTNeurona
+	{
+		float* pesos;
+		int label;
+
+	};
+	typedef struct sTNeurona TNeurona;
+
+	
+	struct sTSOM
+	{
+		int Ancho;
+		int Alto;
+		int Dimension;
+		TNeurona** Neurona;
+	};
+	typedef struct sTSOM TSOM;
+
+	struct sTPatrones
+	{
+		int Cantidad;
+		int Dimension;
+		float ** Pesos;
+	};
+	typedef struct sTPatrones TPatrones;
+
+	
+	/*============================================================================ */
+	/* Variables Globales										                   */
+	/*============================================================================ */
+	TSOM SOM;
+	TPatrones Patrones;
+
+	int* EtiquetaCPU;
+	int* EtiquetaGPU;
+		
+	/*============================================================================ */
+	/* Funciones de tratamiento de memoria							 */
+	/*============================================================================ */
+	void BorrarMapa(void)
+	{
+		int i;
+		if (SOM.Neurona != NULL)
+		{
+			for (i = 0; i < SOM.Ancho; i++)
+			if (SOM.Neurona[i] != NULL) free(SOM.Neurona[i]);
+			free(SOM.Neurona);
+			SOM.Neurona = NULL;
+		}
+	}
+
+	void BorrarPatrones(void)
+	{
+		int i;
+		if (Patrones.Pesos != NULL)
+		{
+			for (i = 0; i < Patrones.Cantidad; i++)
+			if (Patrones.Pesos[i] != NULL) free(Patrones.Pesos[i]);
+			free(Patrones.Pesos);
+			Patrones.Pesos = NULL;
+		}
+	}
+
+	int CrearMapa(int Alto, int Ancho, int Dimension)
+	{
+		int i,j;
+		SOM.Alto = Alto;
+		SOM.Ancho = Ancho;
+		SOM.Neurona = (TNeurona**)malloc(SOM.Alto*sizeof(TNeurona*));
+		if (SOM.Neurona == NULL) return ERRORCLASS;
+		for (j = 0; j < SOM.Alto; j++)
+		{
+			SOM.Neurona[j] = (TNeurona*)malloc(SOM.Ancho*(int)sizeof(TNeurona));
+			if (SOM.Neurona[j] == NULL)
+			{
+				BorrarMapa();
+				return ERRORCLASS;
+			}
+			for (i = 0; i < SOM.Ancho; i++)
+				SOM.Neurona[j][i].pesos = (float*)malloc(Dimension*(int)sizeof(float));
+		}
+		return OKCLAS;
+	}
+
+	int CrearPatrones(int Cantidad, int Dimension)
+	{
+		int j;
+		Patrones.Cantidad=Cantidad;
+		Patrones.Dimension=Dimension;
+		Patrones.Pesos = (float**)malloc(Cantidad*sizeof(float*));
+		for (j = 0; j < Cantidad; j++)
+		{
+			Patrones.Pesos[j] = (float*)malloc(Dimension*(int)sizeof(float));
+			if (Patrones.Pesos[j] == NULL)
+			{
+				BorrarPatrones();
+				return ERRORCLASS;
+			}
+			
+		}
+		return OKCLAS;
+	}
+	float CalculaDistancia(int y, int x, int np)
+	{
+		float distancia=0;
+		if (y>=0 && y<SOM.Alto && x>=0 && x<SOM.Ancho)
+		{
+		for (int i=0;i<Patrones.Dimension;i++)
+			distancia+=abs(SOM.Neurona[y][x].pesos[i]-Patrones.Pesos[np][i]);
+		distancia/=Patrones.Dimension;
+		}
+		return distancia;
+	}
+
+
+#endif // _CLASIFICASOM_H_
+
